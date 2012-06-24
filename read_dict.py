e335d6db7d754c446d2c1789891f06b7377be599
root <root@ip-10-144-11-13.ec2.internal>
2012-06-24 21:34:38 +0000

    Added the first version of the dictionary

--- read_dict.py
+++ read_dict.py
@@ -1,29 +1,15 @@
-﻿from xml.dom.minidom import parse
+from xml.dom.minidom import parse
 from nltk.corpus import wordnet
 
 #parse ankur-abhidhan.xml
 fileName="english_bangla_datasets/ankur-abhidhan.xml"
+fileDict="bangla_dicts/bangla.dict"
 
 debugMode=True
 
 bangla_dict={}
 eng_bangla_dictionary={}
 bangla_eng_dictionary={}
-
-'''
-Sample Entry
-        <row>
-		<field name="id">80142</field>
-		<field name="en_word">cooch.behar</field>
-		<field name="pos_tag">NP</field>
-		<field name="en_lemma"></field>
-		<field name="bn_pronunciation"></field>
-		<field name="bn_word">কুচবেহার</field>
-		<field name="explanation"></field>
-		<field name="example"></field>
-		<field name="status">UNEDITED:TIME=1280170484:NEW_ENG_WORD</field>
-	</row>
-'''
 
 dom=parse(fileName)
 print "Parsed the XML file"
@@ -32,38 +18,43 @@ rows=dom.getElementsByTagName("row")
 
 #Parse the xml into the dictionary eng_bangla_dictionary structure, which will be used later.
 for row in rows:
-	fields=row.getElementsByTagName("field")
-	entry=False
-	for f in fields:
-		if f.getAttribute("name")=="status":
-			if f.childNodes[0].toxml() != "EMPTY" or f.childNodes[0].toxml()!='OBSOLETE':
-				entry=True
-				
-	if entry:
+	try:
+		fields=row.getElementsByTagName("field")
+		entry=False
 		for f in fields:
-			if f.getAttribute("name")=="en_word":
-				en_word=f.childNodes[0].toxml()
+			if f.getAttribute("name")=="status":
+				if f.childNodes[0].toxml() != "EMPTY" or f.childNodes[0].toxml()!='OBSOLETE':
+					entry=True
+				
+		if entry:
+			for f in fields:
+				if f.getAttribute("name")=="en_word":
+					en_word=f.childNodes[0].toxml()
 
-			elif f.getAttribute("name")=="pos_tag":
-				pos_tag=f.childNodes[0].toxml()
+				elif f.getAttribute("name")=="pos_tag":
+					pos_tag=f.childNodes[0].toxml()
 
-			elif f.getAttribute("name")=="bn_word":
-				bn_word=f.childNodes[0].toxml()
+				elif f.getAttribute("name")=="bn_word":
+					bn_word=f.childNodes[0].toxml()
 
-		#specifies key and value for dictionary
-		key=en_word
-		value=(pos_tag,bn_word)
-		#Check if the key already exists or not. If it does append the value, else intialize the array as well.
-		if eng_bangla_dictionary.has_key(key)==False:
-			eng_bangla_dictionary[key]=[]
-		eng_bangla_dictionary[key]=value
+			#specifies key and value for dictionary
+			key=en_word
+			value=(pos_tag,bn_word)
+
+			#Check if the key already exists or not. If it does append the value, else intialize the array as well.
+			if eng_bangla_dictionary.has_key(key)==False:
+				eng_bangla_dictionary[key]=[]
+			eng_bangla_dictionary[key]=value
 		
-		key=bn_word
-		value=(pos_tag,en_word)
-		#Check if the key already exists or not. If it does append the value, else intialize the array as well.
-		if bangla_eng_dictionary.has_key(key)==False:
-			bangla_eng_dictionary[key]=[]
-		bangla_eng_dictionary[key]=value
+			key=bn_word
+			value=(pos_tag,en_word)
+
+			#Check if the key already exists or not. If it does append the value, else intialize the array as well.
+			if bangla_eng_dictionary.has_key(key)==False:
+				bangla_eng_dictionary[key]=[]
+			bangla_eng_dictionary[key]=value
+	except:
+		pass
 
 print "Parsed the Bilingual dictionary into two dictionary structures"
 
@@ -82,7 +73,8 @@ for i,bn_word in enumerate(bangla_eng_dictionary.keys()):
         for s in syns:
                 try:
                         bword=eng_bangla_dictionary[s]
-                        bn_syns.append(bword)
+						#^^ returns a tuple (pos_tag,bn_word)
+                        bn_syns.append(bword[1])
                 except:
                         pass
 
@@ -94,4 +86,21 @@ for i,bn_word in enumerate(bangla_eng_dictionary.keys()):
         #        break
 
 print "Prepared the dictionary."
-#print bangla_dict
+print "Number of words in the dictionary are",len(bangla_dict.keys())
+
+fwrite=open(fileDict,'w')
+lines=[]
+for key in bangla_dict.keys():
+	syns=bangla_dict[key]
+	if not syns:
+		continue
+	#SYNS is an array
+	#value=""
+	#for t in syns:
+	#	value=value+"t"+","	
+	syns=map(lambda s:s.encode('utf-8'),syns)
+	value=",".join(syns)
+	lines.append(str(key.encode('utf-8'))+";"+value+"\n")
+
+fwrite.writelines(lines)
+fwrite.close()
